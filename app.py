5cfbfd2fdc15df5062a33014e0bfa312a81d1986
Prashanna Kumar Yadav <130278308+yprashanna@users.noreply.github.com>
2024-06-29 06:49:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from sentence_transformers import SentenceTransformer, util
 import json
 
 # Load the fine-tuned model
-model = SentenceTransformer('output/trained_model')
+model = SentenceTransformer('fine-tuned-model')
 
 # Load the JSON file
 with open('scholarships.json', 'r') as f:
@@ -30,6 +30,9 @@ def find_answer(question):
                     questions.append(item['question'])
                     answers.append(item['answer'])
 
+    if not questions:
+        return "Sorry, I don't have enough data to answer that question."
+
     # Encode the question and the list of questions
     question_embedding = model.encode(question, convert_to_tensor=True)
     questions_embeddings = model.encode(questions, convert_to_tensor=True)
@@ -39,6 +42,10 @@ def find_answer(question):
 
     # Find the index of the highest score
     best_index = cosine_scores.argmax()
+    best_score = cosine_scores[0][best_index].item()
+
+    if best_score < 0.5:  # Threshold can be adjusted
+        return "Sorry, I couldn't find a relevant answer."
 
     return answers[best_index]
 
@@ -48,5 +55,3 @@ user_question = st.text_input("Ask a question about the scholarship:")
 if user_question:
     answer = find_answer(user_question)
     st.write(f"Answer: {answer}")
-
-
