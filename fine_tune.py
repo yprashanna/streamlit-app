caafe7220c7705f1129f1452f55a1cadc1e2b8a7
Prashanna Kumar Yadav <130278308+yprashanna@users.noreply.github.com>
2024-06-29 07:08:56 +0530

    Update1 fine_tune.py

--- fine_tune.py
+++ fine_tune.py
@@ -9,18 +9,13 @@ with open('scholarships.json', 'r') as f:
 
 # Prepare training examples
 train_examples = []
-required_keys = ['NIT_admissions', 'IIT_admissions', 'political_power', 'education_consultancy', 'college_contact']
-
-for key in required_keys:
-    if key in data:
+for key in data:
+    if key != "description":  # Ensure to skip the description key
         for item in data[key]:
             train_examples.append(InputExample(texts=[item['question'], item['answer']]))
-    else:
-        print(f"Warning: Key '{key}' not found in the JSON file")
 
 # Adding the scheme description to the training examples
-if 'description' in data:
-    train_examples.append(InputExample(texts=["What is COMPEX Scholarship?", data["description"]]))
+train_examples.append(InputExample(texts=["What is COMPEX Scholarship?", data["description"]]))
 
 # Load the model
 model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
